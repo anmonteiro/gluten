1498af8c564cc6bc1044e7f297bd7fae050774e8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-27 19:25:03 -0700

    Refactor the Lwt runtime to reuse more code

--- lwt-unix/ssl_io_dummy.ml
+++ lwt-unix/ssl_io_dummy.ml
@@ -48,8 +48,6 @@ struct
   let shutdown_receive _ = failwith "Ssl not available"
 
   let close _ = failwith "Ssl not available"
-
-  let state _ = failwith "Ssl not available"
 end
 
 let make_default_client _socket = Lwt.fail_with "Ssl not available"
