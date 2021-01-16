56fbc168c0e88335315433625b58f54a429b096b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-16 12:43:12 -0800

    Fix CI and test on OCaml 4.12 (#19)

--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,5 @@
-{ pkgs ? import ./sources.nix { inherit ocamlVersion; }
-, ocamlVersion ? "4_10"
+{ ocamlVersion ? "4_11"
+, pkgs ? import ./sources.nix { inherit ocamlVersion; }
 , doCheck ? true }:
 
 let
