b018a50a0067159dbc4c2a0a4dbfd21486ce0d7e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-01-16 12:48:57 -0800

    sources

--- nix/sources.nix
+++ nix/sources.nix
@@ -3,7 +3,7 @@
 let
   overlays =
     builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/5d510d0f.tar.gz;
+      https://github.com/anmonteiro/nix-overlays/archive/4b56aa5.tar.gz;
 
 in
 
