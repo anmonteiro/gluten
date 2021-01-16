7c0e3f5f0001ca6dc88725443d998b6a4cad66a0
Anurag Soni <anurag@sonianurag.com>
2021-01-16 14:49:22 -0500

    Bigstring is now under core.bigstring_unix (#15)
    Reference: https://github.com/janestreet/core/blob/102863ed50a4e679efea97303d38bd0cceb9eacb/bigstring_unix/src/dune

--- async/gluten_async.ml
+++ async/gluten_async.ml
@@ -221,14 +221,14 @@ module Unix_io :
           (Fd.syscall fd ~nonblocking:true (fun file_descr ->
                Unix.Syscall_result.Int.ok_or_unix_error_exn
                  ~syscall_name:"read"
-                 (Bigstring.read_assume_fd_is_nonblocking
+                 (Bigstring_unix.read_assume_fd_is_nonblocking
                     file_descr
                     bigstring
                     ~pos:off
                     ~len)))
       else
         Fd.syscall_in_thread fd ~name:"read" (fun file_descr ->
-            Bigstring.read file_descr bigstring ~pos:off ~len)
+            Bigstring_unix.read file_descr bigstring ~pos:off ~len)
         >>= fun result -> finish fd buffer result
     in
     go fd bigstring
